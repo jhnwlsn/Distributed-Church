536f5a42739ae329a383eaf59e2ae24fe0fbd781
John Wilson <john@j-wilson.com>
2015-04-25 18:09:35 -0400

    Rearrange poorly written javascript to make sure mountains show on homepage

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,51 +1,4 @@
 // -------------------------------------
-//   Drapes
-// -------------------------------------
-
-var cnvs = document.getElementById("hero-drapes");
-var ctx = cnvs.getContext('2d');
-
-cnvs.height = 300;
-cnvs.width = 2880;
-
-ctx.beginPath();
-ctx.moveTo(0, Math.floor(Math.random() * 110) + 190);
-ctx.lineTo(720, Math.floor(Math.random() * 110) + 190);
-ctx.lineTo(1440, Math.floor(Math.random() * 110) + 190);
-ctx.lineTo(2160, Math.floor(Math.random() * 110) + 190);
-ctx.lineTo(2880, Math.floor(Math.random() * 110) + 190);
-ctx.lineTo(2880, 400);
-ctx.lineTo(0, 400);
-ctx.closePath();
-ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-ctx.fill();
-
-ctx.beginPath();
-ctx.moveTo(0, Math.floor(Math.random() * 200) + 50);
-ctx.lineTo(720, Math.floor(Math.random() * 200) + 50);
-ctx.lineTo(1440, Math.floor(Math.random() * 200) + 50);
-ctx.lineTo(2160, Math.floor(Math.random() * 200) + 50);
-ctx.lineTo(2880, Math.floor(Math.random() * 200) + 50);
-ctx.lineTo(2880, 400);
-ctx.lineTo(0, 400);
-ctx.closePath();
-ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
-ctx.fill();
-
-ctx.beginPath();
-ctx.moveTo(0, Math.floor(Math.random() * 200));
-ctx.lineTo(720, Math.floor(Math.random() * 200));
-ctx.lineTo(1440, Math.floor(Math.random() * 200));
-ctx.lineTo(2160, Math.floor(Math.random() * 200));
-ctx.lineTo(2880, Math.floor(Math.random() * 200));
-ctx.lineTo(2880, 400);
-ctx.lineTo(0, 400);
-ctx.closePath();
-ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
-ctx.fill();
-
-
-// -------------------------------------
 //   Mountains
 // -------------------------------------
 
@@ -90,3 +43,49 @@ context.lineTo(0, 400);
 context.closePath();
 context.fillStyle = 'rgba(240, 241, 242, 0.5)';
 context.fill();
+
+// -------------------------------------
+//   Drapes
+// -------------------------------------
+
+var cnvs = document.getElementById("hero-drapes");
+var ctx = cnvs.getContext('2d');
+
+cnvs.height = 300;
+cnvs.width = 2880;
+
+ctx.beginPath();
+ctx.moveTo(0, Math.floor(Math.random() * 110) + 190);
+ctx.lineTo(720, Math.floor(Math.random() * 110) + 190);
+ctx.lineTo(1440, Math.floor(Math.random() * 110) + 190);
+ctx.lineTo(2160, Math.floor(Math.random() * 110) + 190);
+ctx.lineTo(2880, Math.floor(Math.random() * 110) + 190);
+ctx.lineTo(2880, 400);
+ctx.lineTo(0, 400);
+ctx.closePath();
+ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(0, Math.floor(Math.random() * 200) + 50);
+ctx.lineTo(720, Math.floor(Math.random() * 200) + 50);
+ctx.lineTo(1440, Math.floor(Math.random() * 200) + 50);
+ctx.lineTo(2160, Math.floor(Math.random() * 200) + 50);
+ctx.lineTo(2880, Math.floor(Math.random() * 200) + 50);
+ctx.lineTo(2880, 400);
+ctx.lineTo(0, 400);
+ctx.closePath();
+ctx.fillStyle = 'rgba(255, 255, 255, 0.75)';
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(0, Math.floor(Math.random() * 200));
+ctx.lineTo(720, Math.floor(Math.random() * 200));
+ctx.lineTo(1440, Math.floor(Math.random() * 200));
+ctx.lineTo(2160, Math.floor(Math.random() * 200));
+ctx.lineTo(2880, Math.floor(Math.random() * 200));
+ctx.lineTo(2880, 400);
+ctx.lineTo(0, 400);
+ctx.closePath();
+ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+ctx.fill();
