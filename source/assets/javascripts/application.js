0bf11d19d5893b24e21f04560c4916473425b8dc
John Wilson <john@j-wilson.com>
2015-05-01 22:41:20 -0400

    Start learning how to implement animations

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,46 +1,57 @@
+window.requestAnimFrame = (function(callback) {
+  return window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  function(callback) {
+    window.setTimeout(callback, 1000/ 60);
+  }
+})();
+
+function animate(length, duration) {
+  // 1. Define canvas and context
+  // 2. Clear
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  // 3. Update
+  // 4. Draw
+  // 5. Start all over (request new frame)
+  requestAnimFrame(function() {
+    animate();
+  });
+};
+
+function drawMountain(context) {
+  context.beginPath();
+  for (var i = 0; i <= canvas.width; i += (canvas.width / 4) ) {
+    if (i == 0) {
+      context.moveTo(i, randomNumber(125, 300));
+    } else {
+      context.lineTo(i, randomNumber(125, 300));
+    }
+  }
+  context.lineTo(canvas.width, canvas.height);
+  context.lineTo(0, canvas.height);
+  context.closePath();
+  context.fillStyle = 'rgba(240, 241, 242, 1)';
+  context.fill();
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// =================================
+//  Implementation
+// =================================
+
 var mountains = document.getElementsByClassName("mountains");
 for (var i = 0; i < mountains.length; i++) {
   var canvas = mountains[i];
-  draw_mountain();
-}
-
-function draw_mountain() {
   var context = canvas.getContext('2d');
 
   canvas.height = 300;
   canvas.width = 2880;
 
-  context.beginPath();
-  for (var i = 0; i <= 2880; i += 720 ) {
-    context.lineTo(i, random_number(125, 300));
-  }
-  context.lineTo(2880, 400);
-  context.lineTo(0, 400);
-  context.closePath();
-  context.fillStyle = 'rgba(240, 241, 242, 1)';
-  context.fill();
-
-  context.beginPath();
-  for (var i = 0; i <= 2880; i += 720 ) {
-    context.lineTo(i, random_number(75, 250));
-  }
-  context.lineTo(2880, 400);
-  context.lineTo(0, 400);
-  context.closePath();
-  context.fillStyle = 'rgba(240, 241, 242, 0.75)';
-  context.fill();
-
-  context.beginPath();
-  for (var i = 0; i <= 2880; i += 720 ) {
-    context.lineTo(i, random_number(15, 200));
-  }
-  context.lineTo(2880, 400);
-  context.lineTo(0, 400);
-  context.closePath();
-  context.fillStyle = 'rgba(240, 241, 242, 0.5)';
-  context.fill();
-}
-
-function random_number(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  drawMountain(context);
 }
