9d859894f3bc6c29375febee76d0fffef2660436
John Wilson <john@j-wilson.com>
2015-05-06 20:59:38 -0400

    Add support for canvas drapes

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -9,18 +9,43 @@ function draw(orientation, color) {
   for (var i = 0; i < 3; i++) {
     if (i == 0) {
       context.fillStyle = 'rgba('+ color["r"] +','+ color["g"] +','+ color["b"] +',1)';
-      mountainsPoints("small");
+      if (orientation == "mountains") {
+        mountainsPoints("small");
+      } else if (orientation == "drapes") {
+        drapesPoints("small");
+      }
     } else if (i == 1) {
       context.fillStyle = 'rgba('+ color["r"] +','+ color["g"] +','+ color["b"] +',0.75)';
-      mountainsPoints("medium");
+      if (orientation == "mountains") {
+        mountainsPoints("medium");
+      } else if (orientation == "drapes") {
+        drapesPoints("medium");
+      }
     } else {
       context.fillStyle = 'rgba('+ color["r"] +','+ color["g"] +','+ color["b"] +',0.5)';
-      mountainsPoints("large");
+      if (orientation == "mountains") {
+        mountainsPoints("large");
+      } else if (orientation == "drapes") {
+        drapesPoints("large");
+      }
     }
   }
 };
 
 // ---- Create Points ----- //
+
+// Drapes
+
+function drapesPoints(size) {
+  context.moveTo(canvas.width, 0);
+  context.lineTo(0, 0);
+  for (var i = 0; i <= canvas.width; i+=(canvas.width / 4)) {
+    context.lineTo(i, size == "small" ? randomNumber(50, 200) : size == "medium" ? randomNumber(100, 250) : randomNumber(150, 300));
+  }
+  context.lineTo(canvas.width, canvas.height);
+  context.closePath();
+  context.fill();
+}
 
 // Mountains
 
@@ -51,9 +76,10 @@ function randomNumber(min, max) {
 var gray = {r: 240, g: 241, b: 242};
 var red = {r: 229, g: 65, b: 59};
 var teal = {r: 118, g: 195, b: 176};
+var white = {r: 255, g: 255, b: 255};
 var yellow = {r: 210, g: 183, b: 31};
 
-var mountains = document.getElementsByClassName("polygons");
+var mountains = document.getElementsByClassName("polygons--mountains");
 for (var i = 0; i < mountains.length; i++) {
   var canvas = mountains[i];
   var context = canvas.getContext('2d');
@@ -61,5 +87,16 @@ for (var i = 0; i < mountains.length; i++) {
   canvas.height = 300;
   canvas.width = 2880;
 
-  draw("horizontal", gray);
+  draw("mountains", gray);
+}
+
+var mountains = document.getElementsByClassName("polygons--drapes");
+for (var i = 0; i < mountains.length; i++) {
+  var canvas = mountains[i];
+  var context = canvas.getContext('2d');
+
+  canvas.height = 300;
+  canvas.width = 2880;
+
+  draw("drapes", white);
 }
